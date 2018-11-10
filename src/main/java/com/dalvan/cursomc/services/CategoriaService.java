7342e84dcf01658c3a06bce8210dbdb2681d77c0
Dalvan Schmitz <dalvan.schmitz@gmail.com>
2018-11-10 04:07:24 -0200

    metodo delete e tratamento de exceções

--- src/main/java/com/dalvan/cursomc/services/CategoriaService.java
+++ src/main/java/com/dalvan/cursomc/services/CategoriaService.java
@@ -3,32 +3,43 @@ package com.dalvan.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.dalvan.cursomc.domain.Categoria;
 import com.dalvan.cursomc.repositories.CategoriaRepository;
+import com.dalvan.cursomc.services.exception.DataIntegrityException;
 import com.dalvan.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 
 }
